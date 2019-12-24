12606b3b897dbbe87c7df62f1c32901bf46adf52
root <root@glidos.site>
2019-12-24 14:45:40 +0000

    Bring up to date with latest kiwi

--- config.sh
+++ config.sh
@@ -27,29 +27,14 @@ test -f /.profile && . /.profile
 echo "Configure image: [$kiwi_iname]..."
 
 #======================================
-# Mount system filesystems
-#--------------------------------------
-baseMount
-
-#======================================
 # Setup baseproduct link
 #--------------------------------------
 suseSetupProduct
 
 #======================================
-# Add missing gpg keys to rpm
-#--------------------------------------
-suseImportBuildKey
-
-#======================================
 # Activate services
 #--------------------------------------
 suseInsertService sshd
-if [[ ${kiwi_type} =~ oem|vmx ]];then
-    suseInsertService grub_config
-else
-    suseRemoveService grub_config
-fi
 
 #======================================
 # Setup default target, multi-user
@@ -82,27 +67,3 @@ update-alternatives --auto libglx.so
 #------------------------------------------
 rm -rf /usr/share/doc/packages/*
 rm -rf /usr/share/doc/manual/*
-rm -rf /opt/kde*
-
-#======================================
-# only basic version of vim is
-# installed; no syntax highlighting
-#--------------------------------------
-sed -i -e's/^syntax on/" syntax on/' /etc/vimrc
-
-#======================================
-# SuSEconfig
-#--------------------------------------
-suseConfig
-
-#======================================
-# Remove yast if not in use
-#--------------------------------------
-suseRemoveYaST
-
-#======================================
-# Umount kernel filesystems
-#--------------------------------------
-baseCleanMount
-
-exit 0
